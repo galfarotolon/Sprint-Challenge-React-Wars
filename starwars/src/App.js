81b24946b8eeb5bc7b47e471e6ef80fb86c3b3c7
Guillermo Alfaro <galfarotolon@gmail.com>
2020-04-17 10:41:19 -0500

    completed character render

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import './App.css';
 
-const App = () => {
+import React, { useState, useEffect } from "react";
+import './App.css';
+import axios from "axios"
+import Character from "./components/Character"
+
+
+function App() {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +14,53 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+
+
+  const [data, setGetData] = useState([])
+  const [addCount, setAddCount] = useState(1)
+  const [subtractCount, setSubtractCount] = useState(addCount)
+
+
+
+  function addCounter() {
+    setAddCount(addCount + 1)
+  }
+
+  useEffect(() => {
+
+    axios.get(`https://rickandmortyapi.com/api/character/?page=1`)
+
+      .then(res => {
+
+        setGetData(res.data.results)
+        console.log(res.data)
+
+
+      })
+      .catch(err => {
+        console.log("not fetching any info")
+
+      })
+
+  }, [])
+
+
+  const newArr = Object.values(data)
+
+  console.log(newArr)
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Character Cards</h1>
+
+      <Character character={data} />
+
+
     </div>
-  );
+  )
 }
 
 export default App;
+
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,83 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+////STYLES//
+const Title = styled.h1
+    `
+font-size: 3rem;
+
+`
+const Date = styled.div
+    `
+
+margin-top: 1%;
+font-style: italic;
+font-weight: 600;
+margin-bottom: 5%;
+`
+
+const Img = styled.img
+
+    `
+  
+width: 70%;
+`
+
+const Copyright = styled.div
+    `
+margin-top: 1%;
+font-style: italic;
+font-weight: 600;
+margin-bottom: 5%;
+
+`
+
+const Explanation = styled.div
+    `
+margin-top: 1%;
+line-height: 2;
+padding-bottom: 5%;
+font-weight: 600;
+font-size: 1rem;
+`
+
+//////
+
+
+export default function Character({ character }) {
+
+
+    const newArr = Object.values(character)
+
+    console.log(newArr)
+
+
+    // const img = cardData.hdurl
+    // console.log(img)
+    // console.log(cardData)
+
+
+    return (
+
+        <div>
+
+            {
+                newArr.map(character => {
+                    return (
+                        <div>
+                            <h1>{character.name}</h1>
+                            <p>{character.status}</p>
+                            <p>{character.gender}</p>
+                            <p>{character.origin.name}</p>
+
+                        </div>
+                    )
+                })
+            }
+
+        </div>
+    );
+}
+
